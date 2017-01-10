0adb9c9cf314211d14937bdfe7bae2503dcca164
Sean Naren <seannaren@hotmail.com>
2017-01-10 11:49:57 +0000

    Simplified criterion to use aeon's formatted outputs

--- CTCLoss.py
+++ CTCLoss.py
@@ -6,24 +6,17 @@ from torch.autograd import Function
 
 class CPU_CTC(Function):
     def forward(self, input, target, sizes, label_lens):
+        """
+        input: Tensor of (seqLength x batch x outputDim) containing output from network
+        target: 1 dimensional Tensor containing all the targets of the batch in one large sequence
+        sizes: Tensor of size (batch) containing size of each output sequence from the network
+        label_lens: Tensor of (batch) containing label length of each example
+        """
         is_cuda = True if input.is_cuda else False
         self.target = target
         self.sizes = sizes
         self.label_lens = label_lens
         act_lens = sizes
-        label_lens = torch.FloatTensor(target.size(0))
-        target = target.cpu().numpy()
-        targets = []
-        for i in xrange(len(target)):
-            x = target[i]
-            label = x[x != 0]  # Due to padding, we remove zeros
-            targets.append(torch.FloatTensor(label))
-            label_lens[i] = label.size
-        labels = torch.zeros(int(torch.sum(label_lens)))
-        pos = 0
-        for target in targets:
-            labels[pos:pos + target.size(0)] = target
-            pos = pos + target.size(0)
         acts = input.cpu().numpy()
         act_lens = act_lens.numpy()
         labels = labels.numpy()
