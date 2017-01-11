1e47e72db3b37937497d6fc61a7f17045ddcd2a5
SeanNaren <seannaren@hotmail.com>
2017-01-11 08:33:44 +0000

    Added measures

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import torch
 from aeon import DataLoader
 from neon.backends import gen_backend
@@ -18,7 +19,8 @@ parser.add_argument('--batch_size', default=20, type=int, help='Batch size for t
 parser.add_argument('--max_transcript_length', default=1300, type=int, help='Maximum size of transcript in training')
 parser.add_argument('--frame_length', default=.02, type=float, help='Window size for spectrogram in seconds')
 parser.add_argument('--frame_stride', default=.01, type=float, help='Window stride for spectrogram in seconds')
-parser.add_argument('--max_duration', default=15, type=float, help='The maximum duration of the training data in seconds')
+parser.add_argument('--max_duration', default=15, type=float,
+                    help='The maximum duration of the training data in seconds')
 parser.add_argument('--window', default='hamming', help='Window type for spectrogram generation')
 parser.add_argument('--noise_probability', default=0.4, type=float, help='Window type for spectrogram generation')
 parser.add_argument('--noise_min', default=0.5, type=float, help='Minimum noise to add')
@@ -77,10 +79,36 @@ if args.cuda:
 optimizer = torch.optim.SGD(model.parameters(), args.lr,
                             momentum=args.momentum)
 
-for x in xrange(args.epochs - 1):
-    model.train()
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+batch_time = AverageMeter()
+data_time = AverageMeter()
+losses = AverageMeter()
+
+for epoch in xrange(args.epochs - 1):
+    model.train()
+    end = time.time()
     for i, (data) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
         input = data[0].reshape(minibatch_size, 1, spect_size,
                                 -1)  # Puts the data into the form of batch x channels x freq x time
         # TODO we could probably use the valid percentage to find out the real size
@@ -102,8 +130,19 @@ for x in xrange(args.epochs - 1):
 
         sizes = Variable(torch.FloatTensor(out.size(1)).fill_(out.size(0)), requires_grad=False)
         loss = ctc_loss(out, target, sizes, label_lengths)
-        print(x, loss)
+        losses.update(loss.data[0], input.size(0))
         # compute gradient and do SGD step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        print('Epoch: [{0}][{1}/{2}]\t'
+              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+              'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+            epoch, i, train_loader.nbatches, batch_time=batch_time,
+            data_time=data_time, loss=losses))
