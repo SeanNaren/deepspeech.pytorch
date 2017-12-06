76689bb75e247063f0599b3f0c080a4a4de33736
SeanNaren <seannaren@hotmail.com>
2017-12-06 11:01:12 +0000

    Added dim to softmax due to new pytorch release

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class InferenceBatchSoftmax(nn.Module):
     def forward(self, input_):
         if not self.training:
             batch_size = input_.size()[0]
-            return torch.stack([F.softmax(input_[i]) for i in range(batch_size)], 0)
+            return torch.stack([F.softmax(input_[i], dim=1) for i in range(batch_size)], 0)
         else:
             return input_
 
