291bd2b33fde8a45a3a0d930ee19676f66e31965
ankitmundada <aomundada@gmail.com>
2018-02-01 19:19:54 +0530

    Fixes incorrect batch_size calculation

--- tune_decoder.py
+++ tune_decoder.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
                                       normalize=True)
 
     logits = np.load(args.logits)
-    batch_size = logits[0][0].shape[1]
+    batch_size = logits[0][0].shape[0]
 
     results = []
 
