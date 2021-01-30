f52e9902fcb2f5ea3513e56c5144cfcfbd0801bd
Sean Naren <sean.narenthiran@gmail.com>
2021-01-30 14:09:29 +0000

    Update to RC pretrained models

--- tests/pretrained_smoke_test.py
+++ tests/pretrained_smoke_test.py
@@ -8,9 +8,9 @@ from deepspeech_pytorch.enums import DecoderType
 from tests.smoke_test import DatasetConfig, DeepSpeechSmokeTest
 
 pretrained_urls = [
-    'https://github.com/SeanNaren/deepspeech.pytorch/releases/latest/download/an4_pretrained_v3.ckpt',
-    'https://github.com/SeanNaren/deepspeech.pytorch/releases/latest/download/librispeech_pretrained_v3.ckpt',
-    'https://github.com/SeanNaren/deepspeech.pytorch/releases/latest/download/ted_pretrained_v3.ckpt'
+    'https://github.com/SeanNaren/deepspeech.pytorch/releases/download/V3rc0/an4_pretrained_v3.ckpt',
+    'https://github.com/SeanNaren/deepspeech.pytorch/releases/download/V3rc0/librispeech_pretrained_v3.ckpt',
+    'https://github.com/SeanNaren/deepspeech.pytorch/releases/download/V3rc0/ted_pretrained_v3.ckpt'
 ]
 
 lm_path = 'http://www.openslr.org/resources/11/3-gram.pruned.3e-7.arpa.gz'
